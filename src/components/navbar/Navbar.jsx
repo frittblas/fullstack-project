fa8201f813867b336157c4a5fbf6eb236375bf96
p33t00 <saip8tha@gmail.com>
2023-04-05 23:59:24 +0200

    conditional display of pos toggler

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import {default as ReactNavbar} from 'react-bootstrap/Navbar';
 import './Navbar.css';
 
@@ -20,8 +20,12 @@ export default function Navbar() {
             className="d-inline-block align-top"
           />{' '}
           Forum
-          </ReactNavbar.Brand>
-        <PostToggler />
+        </ReactNavbar.Brand>
+        {
+          useLocation().pathname === '/user' ?
+          <PostToggler />:
+          ""
+        }
         <div className="d-flex ms-auto">
           <Nav.Link as={Link} to="/about">Logout</Nav.Link>
         </div>

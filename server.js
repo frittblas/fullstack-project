4f28557d6d7c90ffc8bcd7daa186dce75273eeab
robin <robin.cobanov@hotmail.com>
2023-04-06 15:15:23 +0200

    Basic router and routes for users and posts

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import userRouter from './routes/userRouter.js';
+import postRouter from './routes/postRouter.js';
 
 dotenv.config();
 
@@ -10,6 +12,9 @@ const app = express();
 app.use(cors()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/users', userRouter);
+app.use('/api/posts', postRouter);
 
 
 mongoose.connect(process.env.DB_URI)

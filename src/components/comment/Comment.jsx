40b9883dba62ece7456ef78627b05b8c3789b32c
p33t00 <saip8tha@gmail.com>
2023-04-07 20:43:51 +0200

    Added Tooltip.

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
 import ListGroup from 'react-bootstrap/ListGroup';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 
-export default function Comment() {
+export default function Comment({id}) {
   return (
     <ListGroup.Item className="p-3">
-      <Image roundedCircle="true" src="../../../public/user.jpeg" height="50" className="float-start me-4" />
+      <OverlayTrigger
+        placement="top"
+        overlay={
+          <Tooltip>
+            <p>John Smith</p>
+          </Tooltip>
+        }
+      >
+        <Image roundedCircle="true" src="../../../public/user.jpeg" height="50" className="float-start me-4" />
+      </OverlayTrigger>
       <p>
       Cras justo odio, Cras justo odio Cras justo odio Cras justo odio Cras justo odio Cras justo odio
       Cras justo odio, Cras justo odio Cras justo odio Cras justo odio Cras justo odio Cras justo odio

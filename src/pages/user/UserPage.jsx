2a302f1f417faa9cb4e01d2bfd6b3b5f81edb92a
p33t00 <saip8tha@gmail.com>
2023-04-11 20:54:30 +0200

    Done with dynamic content load on user page

--- src/pages/user/UserPage.jsx
+++ src/pages/user/UserPage.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import PostForm from '../../components/post-form/PostForm';
 import Post from '../../components/post/Post';
+import { getPosts } from '../../services/api';
 import './UserPage.css';
 
 export default function UserPage() {
+  const [getPostList, setPostList] = useState([])
+
+  useEffect(() => {
+    (async () => {setPostList(await getPosts())})()
+  }, []);
+
   return (
     <div>
-      <PostForm onPostPosted={data => onPostPostedHandler(data)} />
-      <Container className="post-list">
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+      <PostForm onPostPosted={data => setPostList([data].concat(getPostList))} />
+      <Container id="post-list">
+        {getPostList.map(p => <Post postData={p} />)}
       </Container>
     </div>
   );
-}
-
-function onPostPostedHandler(data) {
-  console.log(data)
 }

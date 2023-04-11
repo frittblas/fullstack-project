da4fccdc23a1baae3c6d4ec838b638cf5ba8a281
p33t00 <saip8tha@gmail.com>
2023-04-11 13:59:00 +0200

    raw post request.

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -5,7 +5,7 @@ import './PostForm.css';
 
 export default function PostForm() {
   return (
-    <Form id="post-form" method="POST">
+    <Form id="post-form" method="POST" onSubmit={onPostSubmit}>
       <Form.Group className="mb-3">
         <Form.Control as="textarea" placeholder="..." />
         <Form.Text className="text-muted">
@@ -18,4 +18,35 @@ export default function PostForm() {
       </Form.Group>
     </Form>
   );
+}
+
+async function onPostSubmit(event) {
+  event.preventDefault();
+  const file = event.target[1].files[0];
+  const postData = {};
+
+  postData.author = "someUsername"
+  postData.message = event.target[0].value;
+  postData.program = "Software Development";
+
+  if (file) {
+    postData.image = await toBase64(file);
+  }
+
+  console.log(postData);
+}
+
+function toBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = e => { 
+      const fileData = {name: file.name, type: file.type, size: file.size};
+      const fcontents = e.target.result;
+      const b64Encoded = fcontents.slice(fcontents.indexOf('base64,') + 7);
+      fileData.contents = b64Encoded;
+      resolve(fileData);
+    };
+    reader.onerror = e => reject(e);
+    reader.readAsDataURL(file);
+  });
 }

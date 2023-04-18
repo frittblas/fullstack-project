00f996fa7f20dd58713dfed9c67fc000f670f188
p33t00 <saip8tha@gmail.com>
2023-04-18 19:13:34 +0200

    Done with post toggler.

--- src/pages/users/UsersPage.jsx
+++ src/pages/users/UsersPage.jsx
@@ -3,9 +3,11 @@ import Container from 'react-bootstrap/Container';
 import PostForm from '../../components/post-form/PostForm';
 import Post from '../../components/post/Post';
 import { getPosts } from '../../services/api';
+import { useStates } from 'react-easier';
 import './UsersPage.css';
 
 export default function UsersPage() {
+  const state = useStates("main");
   const [getPostList, setPostList] = useState([])
 
   useEffect(() => {
@@ -16,7 +18,11 @@ export default function UsersPage() {
     <div>
       <PostForm onPostPosted={data => setPostList([data].concat(getPostList))} />
       <Container id="post-list">
-        {getPostList.map(p => <Post postData={p} />)}
+        {
+          state.program === 'All' ? 
+          getPostList.map(p => <Post postData={p} />) :
+          getPostList.filter(p => p.program === state.program).map(p => <Post postData={p} />)
+        }
       </Container>
     </div>
   );

fe9322b3b55e99382170b14b78610c2f40ad87eb
p33t00 <saip8tha@gmail.com>
2023-04-26 17:31:16 +0200

    Create Posts all=1 done

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MAIN_POST_THREAD_NAME } from './../../constants.js';
 import Container from 'react-bootstrap/Container';
 import Navbar from '../navbar/Navbar'
 import Footer from '../footer/Footer';
@@ -9,15 +10,13 @@ import Login from '../../pages/login/Login';
 import Register from '../../pages/register/Register';
 import { Routes, Route, useLocation } from "react-router-dom";
 import { useStates } from 'react-easier';
-
 import './App.css';
-
 
 export default function App() {
   const state = useStates("main", {
-    // dummy programsList
+    // dummy programsList (pick from api in Navbar when in admin page)
     programsList: [{ "_id": "64369652372d6ab6b4c15118", "programTitle": "Software Development" }, { "_id": "64369669372d6ab6b4c15119", "programTitle": "Economics" }, { "_id": "64369674372d6ab6b4c1511a", "programTitle": "IoT Engineers" }, { "_id": "64369683372d6ab6b4c1511c", "programTitle": "Business Administration" }, { "_id": "643696b4372d6ab6b4c1511f", "programTitle": "Digital Design" }, { "_id": "643696da372d6ab6b4c15120", "programTitle": "Food and Meal Science" }],
-    program: 'All'
+    program: MAIN_POST_THREAD_NAME
   });
   const location = useLocation();
   const hideNavbar = location.pathname === '/login' || location.pathname === '/register';

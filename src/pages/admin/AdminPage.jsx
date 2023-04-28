08d1075962661d1cac237819bc5ef4f086285323
roseygirlforra <gulcinkanat97@gmail.com>
2023-04-28 14:39:21 +0200

    add checkboxes, delete button, delete checked user on frontend

--- src/pages/admin/AdminPage.jsx
+++ src/pages/admin/AdminPage.jsx
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Table from 'react-bootstrap/Table';
-import Spinner from 'react-bootstrap/Spinner'
+import Spinner from 'react-bootstrap/Spinner';
 import UserListItem from '../../components/user-list-item/UserListItem';
 import { getUsers, getPrograms } from '../../services/api';
 import { useStates } from 'react-easier';
@@ -15,6 +15,7 @@ export default function AdminPage() {
   const [getCurrentProg, setCurrentProg] = useState('All');
   const [getMainUserList, setMainUserList] = useState([]);
   const [getUsersList, setUsersList] = useState([]);
+  const [selectedUsers, setSelectedUsers] = useState([]);
 
   useEffect(() => {
     (async () => {
@@ -22,9 +23,24 @@ export default function AdminPage() {
       setMainUserList(users);
       setUsersList(users);
       setInitLoad(false);
-    })(setMainUserList, setUsersList, setInitLoad);
+    })();
   }, []);
 
+  const handleSelectUser = (userId) => {
+    const index = selectedUsers.indexOf(userId);
+    if (index > -1) {
+      setSelectedUsers(selectedUsers.filter(id => id !== userId));
+    } else {
+      setSelectedUsers([...selectedUsers, userId]);
+    }
+  };
+
+  const handleDeleteUsers = () => {
+    const updatedUserList = getMainUserList.filter(u => !selectedUsers.includes(u._id));
+    setUsersList(updatedUserList);
+    setSelectedUsers([]);
+  };
+  
   if (state.program !== getCurrentProg) {
     resetSearchField();
     setCurrentProg(state.program);
@@ -36,42 +52,70 @@ export default function AdminPage() {
     <div>
       <Form.Group className="d-flex">
         <Form.Control id="userSearch" type="text" placeholder="first, last, username" />
-        <Button variant="success" 
-          onClick={() => setUsersList(filterUsers(userSearch.value, getUsersList))} 
-          type="submit">
-            Search
+        <Button
+          variant="success"
+          onClick={() => setUsersList(filterUsers(userSearch.value, getUsersList))}
+          type="submit"
+        >
+          Search
         </Button>
-        <Button variant="disabled" 
-          onClick={() => setCurrentProg('')} 
+        <Button
+          variant="disabled"
+          onClick={() => setCurrentProg('')}
           type="reset"
-          className="ms-1">
-            Reset
+          className="ms-1"
+        >
+          Reset
         </Button>
       </Form.Group>
-      {
-        isInitLoad ?
-        <div className="spinner-wrap"><Spinner animation="border" /></div> :
+      <div className="d-flex mb-3">
+        <Button
+          variant="danger"
+          onClick={() => handleDeleteUsers()}
+          type="button"
+          className="delete ms-1"
+          disabled={selectedUsers.length === 0}
+        >
+          Delete
+        </Button>
+      </div>
+      {isInitLoad ? (
+        <div className="spinner-wrap">
+          <Spinner animation="border" />
+        </div>
+      ) : (
         <Table responsive className="align-middle user-table">
           <tbody>
-            {
-              getUsersList.map(u => <UserListItem key={u._id} userData={u}/>)
-            }
+            {getUsersList.map(u => (
+              <UserListItem
+                key={u._id}
+                userData={u}
+                selected={selectedUsers.includes(u._id)}
+                onSelect={() => handleSelectUser(u._id)}
+              />
+            ))}
           </tbody>
         </Table>
-      }
-    </div> 
+      )}
+    </div>
   );
 }
 
 function filterUsers(keyword, userList) {
   const result = userList.filter(u => {
-    return (u.firstname.toLowerCase().includes(keyword.toLowerCase()) 
-      || u.lastname.toLowerCase().includes(keyword.toLowerCase())
-      || u.username.toLowerCase().includes(keyword.toLowerCase())
-  )})
+    return (
+      u.firstname.toLowerCase().includes(keyword.toLowerCase()) ||
+      u.lastname.toLowerCase().includes(keyword.toLowerCase()) ||
+      u.username.toLowerCase().includes(keyword.toLowerCase())
+    );
+  });
+
   return result;
 }
 
-function resetSearchField() {
-  userSearch.value = '';
-}
+
+  function resetSearchField() {
+    const inputField = document.getElementById("userSearch");
+    inputField.value = "";
+  }
+  

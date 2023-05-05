d7859dcbf0500a55c8593800d757d0003344e0d6
robin <robin.cobanov@hotmail.com>
2023-05-05 14:03:55 +0200

    Remove debugging code

--- routes/handlers/statistics.js
+++ routes/handlers/statistics.js
@@ -4,7 +4,6 @@ import User from '../../models/userModel.js';
 
 
 async function getUsersPerProgram() {
-  console.log("hmm")
   try {
     const users = await User.find({}, {profileImg: 0});
     const programs = await Program.find({});
@@ -12,9 +11,9 @@ async function getUsersPerProgram() {
     let resultArray = []
     
     for (let program of programs) {
-      console.log(program.programTitle)
+
       programCounter = 0;
-      console.log(program.programTitle)
+
       for (let user of users) {
         if (user.programTitle === program.programTitle) {
           programCounter++;
@@ -39,9 +38,9 @@ async function getPostsPerProgram() {
     let resultArray = []
     
     for (let program of programs) {
-      console.log(program.programTitle)
+
       programCounter = 0;
-      console.log(program.programTitle)
+
       for (let post of posts) {
         if (post.program === program.programTitle) {
           programCounter++;

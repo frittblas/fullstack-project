c8edcb8f82bd25e1525dfd8c1795b3d9290ec1b0
roseygirlforra <gulcinkanat97@gmail.com>
2023-05-09 14:53:24 +0200

    fixed footer so it sticks to the bottom

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MAIN_POST_THREAD_NAME } from './../../constants.js';
 import Container from 'react-bootstrap/Container';
 import Navbar from '../navbar/Navbar'
@@ -23,24 +23,39 @@ export default function App() {
   const location = useLocation();
   const hideNavbar = location.pathname === '/' || location.pathname === '/login' || location.pathname === '/register';
 
-  return (<>
-    <main>
-      {!hideNavbar && <Navbar />}
-      <Container id="content">
-        <Routes>
-          <Route path="/" element={<Login />}> </Route>
-          <Route path="/users" element={<UsersPage />}> </Route>
-          <Route path="/admin" element={<AdminPage />}> </Route>
-          <Route path="/posts/:postId" element={<PostsPage />}> </Route>
-          <Route path="/login" element={<Login />}> </Route>
-          <Route path="/register" element={<Register />}> </Route>
-          <Route path="/about" element={<About />}> </Route>
-          <Route path="/unauth" element={<Unauthorised />}> </Route>
-          <Route path="/statistics" element={<Statistics />}> </Route>
-        </Routes>
-      </Container>
-    </main>
-    <Footer />
-  </>
+  const [footerVisible, setFooterVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+      const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+      const scrollBottom = documentHeight - (scrollTop + windowHeight);
+      setFooterVisible(scrollBottom < 50);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <div className="app">
+      <main>
+        {!hideNavbar && <Navbar />}
+        <Container id="content">
+          <Routes>
+            <Route path="/" element={<Login />}> </Route>
+            <Route path="/users" element={<UsersPage />}> </Route>
+            <Route path="/admin" element={<AdminPage />}> </Route>
+            <Route path="/posts/:postId" element={<PostsPage />}> </Route>
+            <Route path="/login" element={<Login />}> </Route>
+            <Route path="/register" element={<Register />}> </Route>
+            <Route path="/about" element={<About />}> </Route>
+            <Route path="/unauth" element={<Unauthorised />}> </Route>
+            <Route path="/statistics" element={<Statistics />}> </Route>
+          </Routes>
+        </Container>
+      </main>
+      <Footer className={footerVisible ? 'visible' : 'hidden'} />
+    </div>
   );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
-export default function Footer() {
-  return (<>
-    <footer className="fixed-bottom text-center bg-light text-dark p-3" >
-      <div className="row">
-        <div className="col d-flex align-items-center justify-content-center">
-          © Copyright by Group9&nbsp;&nbsp;&nbsp;
-          <Nav.Link as={Link} to="/about" className="about-link">About us</Nav.Link>
+export default function Footer({ className }) {
+  return (
+    <footer className={`footer ${className}`}>
+      <div className="container">
+        <div className="row">
+          <div className="col d-flex align-items-center justify-content-center">
+            <div className="d-flex align-items-center">
+              <p className="mb-0">
+                © Group9. All rights reserved.&nbsp;&nbsp;&nbsp;
+              </p>
+              <Nav.Link as={Link} to="/about" className="about-link">
+                About us
+              </Nav.Link>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
-  </>
   );
 }

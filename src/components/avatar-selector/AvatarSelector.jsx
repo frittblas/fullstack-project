3be05591526c39abeaf99eb4b55512e36a49cf89
p33t00 <saip8tha@gmail.com>
2023-05-09 20:28:51 +0200

    Implemented avatar selector

--- src/components/avatar-selector/AvatarSelector.jsx
+++ src/components/avatar-selector/AvatarSelector.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Form from 'react-bootstrap/Form';
 import FormRange from 'react-bootstrap/FormRange';
 import Button from 'react-bootstrap/Button';
@@ -7,10 +7,15 @@ import AvatarEditor from 'react-avatar-editor';
 import './AvatarSelector.css';
 
 export default function AvatarSelector({showAvatarSelect, handleCrop, handleClose, handleCancel}) {
+  const editor = useRef(null);
   const [scale, setScale] = useState(1.2);
   const [rawImg, setRawImg] = useState("");
 
-console.log(rawImg);
+  const cropHandler = () => {
+    const croppedBlob = editor.current.getImage().toDataURL();
+    handleCrop(croppedBlob);
+    handleClose();
+  }
 
   return (
     <Modal aria-labelledby="contained-modal-title-vcenter"
@@ -18,31 +23,30 @@ console.log(rawImg);
       show={showAvatarSelect} 
       onHide={() => handleClose()}>
       <Modal.Body>
-      <AvatarEditor
+      <AvatarEditor className="w-100 h-auto"
+        ref={editor}
         image={rawImg}
         borderRadius={1000}
-        width={250}
-        height={250}
         border={50}
-        color={[255, 255, 255, 0.6]} // RGBA
+        color={[255, 255, 255, 0.6]}
         scale={scale}
         rotate={0}
       />
       </Modal.Body>
       <Modal.Footer>
-        <Form.Group>
+        <Form.Group className="w-100">
           <Form.Label>Scale</Form.Label>
           <Form.Range min={0.6} max={10} value={scale} step={0.2} onChange={(e) => setScale(Number(e.target.value))}/>
         </Form.Group>
-        <Form.Group>
-          <input type="file" accept="image/png, image/jpeg, image/jpg" onChange={e => setRawImg(e.target.files[0])} />
+        <Form.Group className="mb-3 w-100">
+          <Form.Control type="file" accept="image/png, image/jpeg, image/jpg" onChange={e => setRawImg(e.target.files[0])} />
         </Form.Group>
         <br/>
-        <Button className="rounded new-user-btn btn-light-green" onClick={() => handleClose()}>Confirm</Button>
-        <Button className="rounded new-user-btn btn-light-green" variant='disabled' 
-          onClick={() => {handleCancel(); handleClose();}}
+        <Button className="rounded new-user-btn btn-light-green" onClick={() => cropHandler(rawImg)}>Confirm</Button>
+        <Button className="rounded" variant='secondary' 
+          onClick={() => {setRawImg(""); setScale(1.2); handleCancel(); handleClose();}}
         >
-          Cancel
+          Clear
         </Button>
       </Modal.Footer>
     </Modal>

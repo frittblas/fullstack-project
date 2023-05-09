754a08b8d728e209df6b604dd287680f9277af35
p33t00 <saip8tha@gmail.com>
2023-05-09 18:00:46 +0200

    image selector implemented

--- src/components/avatar-selector/AvatarSelector.jsx
+++ src/components/avatar-selector/AvatarSelector.jsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import Form from 'react-bootstrap/Form';
+import FormRange from 'react-bootstrap/FormRange';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import Avatar from 'react-avatar-edit';
+import AvatarEditor from 'react-avatar-editor';
 import './AvatarSelector.css';
 
 export default function AvatarSelector({showAvatarSelect, handleCrop, handleClose, handleCancel}) {
-  const [src, setSrc] = useState(null);
-  // const [preview, setPreview] = useState(null);
+  const [scale, setScale] = useState(1.2);
+  const [rawImg, setRawImg] = useState("");
+
+console.log(rawImg);
 
   return (
     <Modal aria-labelledby="contained-modal-title-vcenter"
@@ -14,15 +18,32 @@ export default function AvatarSelector({showAvatarSelect, handleCrop, handleClos
       show={showAvatarSelect} 
       onHide={() => handleClose()}>
       <Modal.Body>
-        <Avatar onClose={() => {setSrc(null); handleCancel();}} onCrop={img => handleCrop(img)} width={300} height={300} src={src}/>
+      <AvatarEditor
+        image={rawImg}
+        borderRadius={1000}
+        width={250}
+        height={250}
+        border={50}
+        color={[255, 255, 255, 0.6]} // RGBA
+        scale={scale}
+        rotate={0}
+      />
       </Modal.Body>
       <Modal.Footer>
-      <Button className="rounded new-user-btn btn-light-green" variant='disabled' 
+        <Form.Group>
+          <Form.Label>Scale</Form.Label>
+          <Form.Range min={0.6} max={10} value={scale} step={0.2} onChange={(e) => setScale(Number(e.target.value))}/>
+        </Form.Group>
+        <Form.Group>
+          <input type="file" accept="image/png, image/jpeg, image/jpg" onChange={e => setRawImg(e.target.files[0])} />
+        </Form.Group>
+        <br/>
+        <Button className="rounded new-user-btn btn-light-green" onClick={() => handleClose()}>Confirm</Button>
+        <Button className="rounded new-user-btn btn-light-green" variant='disabled' 
           onClick={() => {handleCancel(); handleClose();}}
         >
           Cancel
         </Button>
-        <Button className="rounded new-user-btn btn-light-green" onClick={() => handleClose()}>Confirm</Button>
       </Modal.Footer>
     </Modal>
   );
